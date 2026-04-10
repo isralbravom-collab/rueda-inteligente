17348d572e8d7a55ddf3c39901e67a81e42d7d42
isralbravom-collab <isralbravom@gmail.com>
2026-04-09 18:36:46 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,25 +30,11 @@ export default function App() {
             />
           }/>
           <Route path="/plan" element={<Plan rides={store.rides} supps={store.supps} profile={store.profile}/>}/>
-          <Route path="/historial" element={
-            <Historial 
-              rides={store.rides} 
-              deleteRide={store.deleteRide}
-              clearAllRides={store.clearAllRides}
-            />
-          }/>
+          <Route path="/historial" element={<Historial rides={store.rides} deleteRide={store.deleteRide} profile={store.profile}/>}/>
           <Route path="/graficas" element={<Graficas rides={store.rides}/>}/>
           <Route path="/suplementos" element={<Suplementos supps={store.supps} addSupp={store.addSupp} deleteSupp={store.deleteSupp} profile={store.profile} rides={store.rides}/>}/>
           <Route path="/perfil" element={<Perfil profile={store.profile} saveProfile={store.saveProfile}/>}/>
-          <Route path="/strava" element={
-            <Strava
-              rides={store.rides}
-              addRide={store.addRide}
-              isDuplicate={store.isDuplicate}
-              profile={store.profile}
-              clearAllRides={store.clearAllRides}
-            />
-          }/>
+          <Route path="/strava" element={<Strava rides={store.rides} addRide={store.addRide} isDuplicate={store.isDuplicate} profile={store.profile}/>}/>
         </Routes>
       </main>
     </div>
